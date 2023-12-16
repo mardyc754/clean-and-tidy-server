08d6db1104a42d2483e06dd0805de5cb5a8a22bb
Marta Dychała <mar754dyc@gmail.com>
2023-12-16 18:40:29 +0100

    Sort visits ascending by their start date for the reservation queries (#34)

--- src/utils/visits.ts
+++ src/utils/visits.ts
@@ -18,8 +18,17 @@ type VisitQueryResult = Visit & {
 };
 
 export function flattenNestedVisits(visits: VisitQueryResult[]) {
-  return visits.map((visit) => flattenNestedVisit(visit));
+  const newVisits = visits.map((visit) => flattenNestedVisit(visit));
+
+  if (newVisits.length === 0) return newVisits;
+
+  newVisits.sort((a, b) => visitStartDate(a) - visitStartDate(b));
+
+  return newVisits;
 }
+
+const visitStartDate = (visit: ReturnType<typeof flattenNestedVisit>) =>
+  visit.visitParts[0]!.startDate.getTime();
 
 export function flattenNestedReservationServices(
   services: ReservationServiceNested[]
